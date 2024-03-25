a0ccc3edd5dd0b6af911f56246edf3a952285610
Alex Huang <huang08alex@gmail.com>
2024-03-25 18:09:29 -0400

    Completed 1 - 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    let reverseArray = [];
+    for (let i=0; i<strArray.length ; i++){
+        reverseArray[i] = strArray[strArray.length-i-1];
+        console.log (strArray +" reversed "+ reverseArray);
+    }
+    return reverseArray.join('');
 };
 
 // Do not edit below this line
