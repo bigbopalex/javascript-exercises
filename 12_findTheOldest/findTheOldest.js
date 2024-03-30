2a3bb34e4df403b8ae1f7e5f6b36d916dce11889
Alex Huang <huang08alex@gmail.com>
2024-03-30 16:38:14 -0400

    Completed all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let output = people.reduce((oldest, person) => {
+        person.age = 'yearOfDeath' in person ? 
+            person.yearOfDeath - person.yearOfBirth:
+            new Date().getFullYear() - person.yearOfBirth;
+        return oldest.age < person.age || oldest.age === undefined ?
+            oldest = person : oldest;
+    }, 0);
+    return output;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
